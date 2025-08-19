e3b9b633957cdc3c79753aaa3a0781eba66a857e
jhgfhii <shubhamsingh1137@gmail.com>
2025-08-19 14:00:20 +0530

    Rename contact.jsx to Contact.jsx

--- controller/contact.js
+++ controller/contact.js
@@ -1,4 +1,4 @@
-import { Contact } from "../models/contact.js";
+import { Contact } from "../models/Contact.js";
 
 export const getALLContact = async (req, res) => {
   const userContact = await Contact.find();
